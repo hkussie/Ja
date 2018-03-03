f3781ece5e351d1c72b7c23b8bb840f6a56079b1
hkussie <hkussie@gmail.com>
2018-03-03 17:07:19 -0500

    Add tests to spec folder

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,5 +1,21 @@
-describe('hello world', function () {
-	it('hello test', function() {
-		console.log('Hello World');
+//	File must end in .spec.js and saved in the spec folder
+//	Describe block is used to contain multilple tests that naturally belong together 
+//	The actual parameters of the test are written in the it block 
+// 	Should contain string describing what the objective of the test is 
+// 	A describe block can exist within another describe block 
+
+describe('calculate addition', function() {
+	it('should be able to add two numbers together', function() {
+		console.log('Two numbers were added successfully!');
+	});
+
+	it('two numbers added together should be equal to five', function () {
+		console.log('The total was five!');
+	});
+
+	describe('calculate numbers using addition and subtraction', function () {
+		it('A total should be derived from adding two numbers and subtracting another', function () {
+			console.log('Two numbers were added and a single number was subtracted from another.');
+		});
 	});
 });
